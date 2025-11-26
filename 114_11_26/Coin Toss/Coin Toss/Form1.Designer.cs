e5532e43fe599c97a6f409a271b43062100b229e
Bobobohan <a112223017@mail.shu.edu.tw>
2025-11-26 13:03:00 +0800

    114_11_26

---
+++ 114_11_26/Coin Toss/Coin Toss/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Coin_Toss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tailsPictureBox = new System.Windows.Forms.PictureBox();
+            this.headsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tossButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tailsPictureBox
+            // 
+            this.tailsPictureBox.Image = global::Coin_Toss.Properties.Resources.Tails1;
+            this.tailsPictureBox.Location = new System.Drawing.Point(83, 26);
+            this.tailsPictureBox.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.tailsPictureBox.Name = "tailsPictureBox";
+            this.tailsPictureBox.Size = new System.Drawing.Size(425, 366);
+            this.tailsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tailsPictureBox.TabIndex = 1;
+            this.tailsPictureBox.TabStop = false;
+            this.tailsPictureBox.Visible = false;
+            // 
+            // headsPictureBox
+            // 
+            this.headsPictureBox.Image = global::Coin_Toss.Properties.Resources.Heads1;
+            this.headsPictureBox.Location = new System.Drawing.Point(83, 26);
+            this.headsPictureBox.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.headsPictureBox.Name = "headsPictureBox";
+            this.headsPictureBox.Size = new System.Drawing.Size(425, 366);
+            this.headsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.headsPictureBox.TabIndex = 0;
+            this.headsPictureBox.TabStop = false;
+            // 
+            // tossButton
+            // 
+            this.tossButton.Location = new System.Drawing.Point(100, 418);
+            this.tossButton.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.tossButton.Name = "tossButton";
+            this.tossButton.Size = new System.Drawing.Size(187, 50);
+            this.tossButton.TabIndex = 2;
+            this.tossButton.Text = "擲硬幣";
+            this.tossButton.UseVisualStyleBackColor = true;
+            this.tossButton.Click += new System.EventHandler(this.tossButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(303, 418);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(187, 50);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "退出";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 493);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tossButton);
+            this.Controls.Add(this.headsPictureBox);
+            this.Controls.Add(this.tailsPictureBox);
+            this.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "擲硬幣";
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox headsPictureBox;
+        private System.Windows.Forms.PictureBox tailsPictureBox;
+        private System.Windows.Forms.Button tossButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
